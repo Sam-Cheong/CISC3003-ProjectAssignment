b29579ddce525852c0ab38a53abd4784a07f5fd8
Tom <chanhinieong@gmail.com>
2025-04-27 19:51:46 +0800

    Update login.php

--- views/manager/login.php
+++ views/manager/login.php
@@ -115,12 +115,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         
         <form method="post">
             <div class="form-group">
-                <label for="email">Emalil：</label>
+                <label for="email">Email</label>
                 <input type="email" id="email" name="email" required>
             </div>
             
             <div class="form-group">
-                <label for="password">Password：</label>
+                <label for="password">Password</label>
                 <input type="password" id="password" name="password" required>
             </div>
             
