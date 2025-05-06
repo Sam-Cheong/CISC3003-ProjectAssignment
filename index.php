a6fa5a048f2ed79e6cd066d58069c6bf78ede71c
Tom <chanhinieong@gmail.com>
2025-05-06 19:14:08 +0800

    home page update

--- index.php
+++ index.php
@@ -4,21 +4,39 @@ $title = "Home";
 
 require_once __DIR__ . '/helpers/session_helper.php';
 include __DIR__ . '/views/layouts/header.php';
-require_once __DIR__ . '/models/Course.php';  // 新增引入课程模型
-//chnage model to controller
-
-$courseModel = new Course();
-$courses = $courseModel->getAllCourses();  // 获取所有课程
 ?>
 
     <main>
         <section id="hero">
             <div class="hero-container">
-                
+                <div class="hero-left">
+                    <h1>Build every possibility for you </h1>
+                    <p>Discover and develop your potential skills.</p>
+                    <a href="./views/course/" class="btn-3d"><span>View Courses</span></a>
+                </div>
+                <div class="hero-right">
+                    <img src="./public/images/hero.jpg" alt="">
+                </div>
             </div>
         </section>
-        <section id="">
-            <h1 class="visually-hidden">Course Management System</h1>
+        <section id="popular">
+            <div class="popular-container">
+                <h1>Popular Courses</h1>
+                <div class="popular-courses">
+                    <div class="popular-course">
+                        <h2>Course Name</h2>
+                        <h3>Course Code</h3>
+                        <p>Time</p>
+                    </div>
+                    <div class="popular-course">
+                        <h2>Web</h2>
+                    </div>
+                    <div class="popular-course">
+                        <h2>Web</h2>
+                    </div>
+                </div>
+            </div>
+
         </section>
         <section>
 
